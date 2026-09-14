c1fb106780b8e1501d462b4a0d5fea58477a38ec
asifraza-776 <asifazhar92@gmail.com>
2026-09-14 17:22:23 +0530

    style: remove bright cyan colors and make download & caption buttons compact on phone and laptop

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,5 +1,5 @@
 // MelodySphere PWA Service Worker (PWABuilder & Google Play Store Compliant)
-const CACHE_NAME = 'melodysphere-v4';
+const CACHE_NAME = 'melodysphere-v5';
 const OFFLINE_PAGE = '/offline.html';
 
 const APP_SHELL = [
